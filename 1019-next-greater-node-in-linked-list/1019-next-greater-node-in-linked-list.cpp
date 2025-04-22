e74b5397a47175af1473bc6909ee62b56c8f0232
Anshuman Singh <123056613+Anshuman261104@users.noreply.github.com>
2025-04-22 21:55:20 +0530

    Time: 900 ms (8.5%), Space: 45.5 MB (80.1%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int>k;
+        while (head!=NULL){
+            ListNode* tra=head;
+            int x=-1;
+            while (tra->next!=NULL){
+                tra=tra->next;
+                if (tra->val>head->val){x=tra->val;break;}
+            }
+            if (x==-1){k.push_back(0);}
+            else {k.push_back(x);}
+            head=head->next;
+        }
+        return k;
+    }
+};
